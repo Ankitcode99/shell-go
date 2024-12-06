7edd0f3765c15b2da39309fe027e92b9b6cb50a3
Ankitcode99 <9999385728ankit@gmail.com>
2024-12-07 00:48:38 +0530

    Added typeHandler

--- cmd/builtins/commands.go
+++ cmd/builtins/commands.go
@@ -5,17 +5,33 @@ import (
 	"os"
 )
 
+var commands = map[string]bool{
+	"exit": true,
+	"type": true,
+	"echo": true,
+}
+
 func BuiltinHandler(cmd, input string) {
 
 	switch cmd {
 	case "exit":
 		exitHandler(input)
-	// case "pwd":
-	// 	PwdHandler()
+	case "type":
+		typeHandler(input)
 	case "echo":
 		echoHandler(input)
 	default:
 		fmt.Printf("%s: command not found\n", input[:len(input)-1])
+	}
+}
+
+func typeHandler(input string) {
+
+	_, exists := commands[input[5:len(input)-1]]
+	if !exists {
+		fmt.Printf("%s: command not found\n", input[5:len(input)-1])
+	} else {
+		fmt.Printf("%s is a shell builtin\n", input[5:len(input)-1])
 	}
 }
 

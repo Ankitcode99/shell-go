c3954ee731c51a8f356ebc6adaeca3e748654f3a
Ankitcode99 <9999385728ankit@gmail.com>
2024-12-07 13:20:07 +0530

    Fixed quote issue

--- cmd/helpers/parse.go
+++ cmd/helpers/parse.go
@@ -6,11 +6,19 @@ func ParseInput(input string) []string {
 	var parts []string
 	var current strings.Builder
 	inQuotes := false
+	quoteChar := ' ' // Track which quote character is currently open
 
 	for _, char := range input {
 		switch char {
 		case '"', '\'':
-			inQuotes = !inQuotes // Toggle quoting state
+			if !inQuotes {
+				inQuotes = true
+				quoteChar = char // Set the current quote character
+			} else if char == quoteChar {
+				inQuotes = false // Close the current quote
+			} else {
+				current.WriteRune(char) // Keep different quote types inside
+			}
 		case ' ':
 			if inQuotes {
 				current.WriteRune(char) // Keep spaces inside quotes
